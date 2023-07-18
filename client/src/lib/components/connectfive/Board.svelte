800787f7d6c4a1f8b724a1f641f595f1eb500510
Simon Wang <mtwangyugeng@gmail.com>
2023-07-17 22:21:47 -0400

    first commit with the board

---
+++ client/src/lib/components/connectfive/Board.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+    import { writable } from "svelte/store";
+
+    const width = 19;
+    const height = 19;
+
+    export const peices = writable(
+        Array(height).fill(null).map( _=> Array(width).fill(0))
+    )
+</script>
+
+<script>
+    import Piece from "./Piece.svelte";
+
+
+</script>
+
+
+<section 
+    class="Board" 
+    style={`grid-template-columns: repeat(${width}, 1fr);
+            grid-template-rows: repeat(${height}, 1fr);`}
+    >
+    {#each $peices as row, i (i)}
+        {#each row as piece, j (j)}
+            <Piece n={i!=0} s={i!=height-1} w={j!=0} e={j!=width-1} i={i} j={j}/>
+        {/each}
+    {/each}
+</section>
+
+<style>
+    .Board {
+        display: grid;
+        
+    }
+</style>
+
---
+++ client/src/lib/components/connectfive/Piece.svelte
@@ -0,0 +1,88 @@
+<script>
+    import { peices } from "./Board.svelte";
+
+    export let demension = "30px";
+    export let n = true, 
+        s = true, 
+        w = true, 
+        e = true;
+
+    /**
+     * @type {number}
+     */
+     export let i;
+    /**
+     * @type {number}
+     */
+     export let j;
+
+
+    function handleClick() {
+        console.log(i, j)
+        peices.update(v => {
+            v[i][j] = 1;
+            return v
+        })
+    }
+</script>
+
+<button class='Piece' style={`height: ${demension};`} on:click={handleClick}>
+    <div class='CrossH'>
+        <div style={`${w && 'background: black'}`} />
+        <div style={`${e && 'background: black'}`} />
+    </div>
+    <div class='CrossV'>
+        <div style={`${n && 'background: black'}`} />
+        <div style={`${s && 'background: black'}`} />
+    </div>
+
+    {#if $peices[i][j] == 1}
+        <div class='Qi'>
+        </div>
+    {/if}
+</button>
+
+
+<style>
+    .Piece {
+        all: unset;
+        aspect-ratio: 1;
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .Piece:hover {
+        background-color: rgb(190, 190, 190);
+    }
+
+    .CrossH {
+        height: 2px;
+        width: 100%;
+        position: absolute;
+        display: flex;
+    }
+    .CrossH div {
+        flex: 1;
+    }
+
+    .CrossV {
+        height: 100%;
+        width: 2px;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+    }
+    .CrossV div {
+        flex: 1;
+    }
+
+    .Qi {
+        background-color: rgb(218, 0, 0);
+        z-index: 2;
+        border-radius: 50%;
+        height: 90%;
+        width: 90%;
+    }
+</style>
