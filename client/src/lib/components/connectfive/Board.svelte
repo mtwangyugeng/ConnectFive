cc4737690078812c1debabe699790a53fc5f58af
Simon Wang <mtwangyugeng@gmail.com>
2023-07-18 23:34:51 -0400

    added winner check algorithm

--- client/src/lib/components/connectfive/Board.svelte
+++ client/src/lib/components/connectfive/Board.svelte
@@ -7,6 +7,76 @@
     export const peices = writable(
         Array(height).fill(null).map( _=> Array(width).fill(0))
     )
+    /**
+     * @param {number} i
+     * @param {number} j
+     * @param {number[][]} board
+     */
+    function checkWin(i, j, board) {
+        const qi = board[i][j];
+        const height = board.length;
+        const width = board[0].length;
+
+        let horizontal = 1;
+        let cnt = j + 1;
+        while (cnt < width && board[i][cnt] === qi) {
+            horizontal ++;
+            cnt++;
+        }
+        cnt = j - 1;
+        while (cnt >= 0 && board[i][cnt] === qi) {
+            horizontal ++;
+            cnt--;
+        }
+
+        let vertical = 1;
+        cnt = i + 1;
+        while (cnt < height && board[cnt][j] === qi) {
+            vertical ++;
+            cnt++;
+        }
+        cnt = i - 1;
+        while (cnt >= 0 && board[cnt][j] === qi) {
+            vertical ++;
+            cnt--;
+        }
+
+        let diagonal = 1;
+        let cntI = i + 1;
+        let cntJ = j + 1;
+        while (cntI < height && cntJ < width && board[cntI][cntJ] === qi) {
+            diagonal ++;
+            cntI++;
+            cntJ++;
+        }
+        cntI = i - 1;
+        cntJ = j - 1;
+        while (cntI >= 0 && cntJ >= 0 && board[cntI][cntJ] === qi) {
+            diagonal ++;
+            cntI--;
+            cntJ--;
+        }
+
+        let diagonalAlter = 1;
+        cntI = i + 1;
+        cntJ = j - 1;
+        while (cntI < height && cntJ >= 0 && board[cntI][cntJ] === qi) {
+            diagonalAlter ++;
+            cntI++;
+            cntJ--;
+        }
+        cntI = i - 1;
+        cntJ = j + 1;
+        while (cntI >= 0 && cntJ < width && board[cntI][cntJ] === qi) {
+            diagonalAlter ++;
+            cntI--;
+            cntJ++;
+        }
+
+        console.log(horizontal, vertical, diagonal, diagonalAlter)
+        return horizontal >= 5 || vertical >=5 || diagonal >= 5 || diagonalAlter >= 5;
+    }
+
     export const curPlayer = writable(1);
     function switchPlayer() {
         curPlayer.update(v=>{
@@ -20,8 +90,10 @@
      * @param {number} curplayer
      */
     function updatePiece(i, j, curplayer) {
+       
         peices.update(v => {
             v[i][j] = curplayer;
+            console.log(i, j, checkWin(i , j, v));
             return v;
         })
 
